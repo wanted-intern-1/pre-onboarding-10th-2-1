4960e3b5e5be76c9520013bbc4fae4ac5dfd7cb8
Woohyun Park <woohyunpark.mail@gmail.com>
2023-05-05 14:32:44 +0900

    refact, api 가독성 향상

--- src/api/keyword.ts
+++ src/api/keyword.ts
@@ -5,10 +5,11 @@ const BASE_URL = '/api/v1/search-conditions';
 const needFetch = async (keyword: string) => {
   const url = `${BASE_URL}/?name=${keyword}`;
   const cache = await caches.open('keywords');
+  const cachedData = await cache.match(url);
 
-  let cachedData = await cache.match(url);
-  if (!cachedData) return true;
-  if (isExpired(cachedData.headers.get('date'))) return true;
+  if (!cachedData || isExpired(cachedData.headers.get('date'))) {
+    return true;
+  }
   return false;
 };
 
@@ -18,10 +19,16 @@ const formatCache = async (cacheData: Response | undefined) => {
 
 const fetchData = async (keyword: string) => {
   if (!keyword) return [];
+
   const url = `${BASE_URL}/?name=${keyword}`;
   const cache = await caches.open('keywords');
-  if (await needFetch(keyword)) await cache.add(url);
-  return formatCache(await cache.match(url));
+
+  if (await needFetch(keyword)) {
+    await cache.add(url);
+  }
+
+  const cachedData = await cache.match(url);
+  return formatCache(cachedData);
 };
 
 const keywordApi = {
